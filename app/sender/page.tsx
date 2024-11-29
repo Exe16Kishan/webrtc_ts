9fe49c390f840e2539f55c19e510b4b0656d13ed
Kishan <kishanshukla53@gmail.com>
2024-11-29 23:17:03 +0530

    commenting

--- app/sender/page.tsx
+++ app/sender/page.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 
 function page() {
-    const[socket,setsocket]=useState<WebSocket | null>(null)
+    const[socket,setsocket]=useState<WebSocket | null>(null) // to store the socket 
   useEffect(() => {
     const socket = new WebSocket("ws://localhost:8080");
     socket.onopen = () => {
@@ -16,9 +16,13 @@ function page() {
   }, []);
 
   async function start(){
+    // create a instance of webrtc
     const pc  = new RTCPeerConnection()
+    // create an offer 
     const offer  = await pc.createOffer()
+    // set it to local decription
     await pc.setLocalDescription(offer)
+    // send the data sdp to the other side through signaling 
     socket?.send(JSON.stringify({type:"createOffer",
         sdp:pc.localDescription
 
