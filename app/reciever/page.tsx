b83af3d0463b27122aef0c4345531b198b0226a2
Kishan <kishanshukla53@gmail.com>
2024-11-29 22:34:56 +0530

    client side

---
+++ app/reciever/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React, { useEffect } from 'react'
+
+function page() {
+    useEffect(()=>{
+
+        const socket = new WebSocket('ws://localhost:8080')
+        socket.onopen=()=>{
+            socket.send(JSON.stringify({
+                type:'receiver'
+            }))
+        }
+
+
+    },[])
+    
+  return (
+    <div>receiver</div>
+  )
+}
+
+export default page
